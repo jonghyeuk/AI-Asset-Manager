941ea4e9506965a0a26608cadac70e3018251603
jonghyeuk <jonghyeuk@gmail.com>
2025-08-29 20:26:08 +0900

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AIAssetManager from './components/AIAssetManager';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AIAssetManager />
+    </div>
+  );
+}
+
+export default App;
